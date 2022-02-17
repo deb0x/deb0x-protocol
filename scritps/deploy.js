1b4214b24caa8394b9c62cfdbf0ab6fadd41182b
Alex Males <alexandru.males@gmail.com>
2022-02-17 20:31:21 +0200

    deploy script

---
+++ scritps/deploy.js
@@ -0,0 +1,15 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    const Deb0x = await ethers.getContractFactory("Deb0x");
+    const deb0x = await Deb0x.deploy();
+    console.log(`Deployed Deb0x at ${deb0x.address}`);
+}
+
+main()
+.then(() => process.exit(0))
+.catch((error) => {
+    console.log(error);
+    process.exit(1);
+});
