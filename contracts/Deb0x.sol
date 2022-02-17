c98fe124df395ac9619df6d213815183a9468f28
Alex Males <alexandru.males@gmail.com>
2022-02-17 21:30:28 +0200

    contract initial implementation

--- contracts/Deb0x.sol
+++ contracts/Deb0x.sol
@@ -4,4 +4,23 @@ pragma solidity ^0.8.11;
 
 contract Deb0x {
 
+    mapping (address => string) private encryptionKeys;
+
+    mapping (address => string[]) private messages;
+
+    function setKey(string memory encryptionKey) public {
+        encryptionKeys[msg.sender] = encryptionKey;
+    }
+
+    function getKey(address account) public view returns (string memory) {
+        return encryptionKeys[account];
+    }
+
+    function send(address to, string memory payload) public {
+        messages[to].push(payload);
+    }
+
+    function fetchMessages(address to) public view returns (string[] memory) {
+        return messages[to];
+    }
 }
