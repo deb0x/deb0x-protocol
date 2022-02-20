edf1e7493f1a86263b5fd584c25e0603f6736390
CatalinBalut <balut.catalin@yahoo.com>
2022-02-20 19:16:24 +0200

    refactored stake, added local balances, created initialize function, set constant total supply for erc 20

--- contracts/Deb0x.sol
+++ contracts/Deb0x.sol
@@ -9,10 +9,27 @@ contract Deb0x {
     Deb0xERC20 public deboxERC20;
 
     uint256 constant public fee = 1000;
+    
+    bool initializeFlag = false;
 
     mapping (address => string) private encryptionKeys;
 
     mapping (address => string[]) private messages;
+
+    mapping (address => uint256) private balance;
+
+    constructor(){
+        //use new to creat Deboxerc20
+    }
+
+    function initialize () public {
+        require(initializeFlag == false, "Deb0x: initialize() can be called just once");
+
+        deboxERC20.approve(address(this), deboxERC20.totalSupply());
+        balance[address(this)] = deboxERC20.totalSupply();
+
+        initializeFlag = true;
+    }
 
     function setKey(string memory encryptionKey) public {
         encryptionKeys[msg.sender] = encryptionKey;
@@ -25,9 +42,29 @@ contract Deb0x {
     function send(address to, string memory payload) public payable {
         require(msg.value >= gasleft() * fee / 10000, "Deb0x: must pay 10% of transaction cost");
 
-        deboxERC20.mint(msg.sender, 7 * (10 * 18));
+        balance[address(this)] -= 7 * (10 * 18);
+        balance[msg.sender] += 7 * (10 * 18);
 
         messages[to].push(payload);
+    }
+
+    function stake(uint256 amount) public {
+        require (amount != 0, "Deb0x: your amount is 0");
+        
+        deboxERC20.approve(address(this), amount);
+        deboxERC20.transferFrom(msg.sender, address(this), amount);
+
+        balance[msg.sender] += amount;
+    }
+
+    function unStake(uint256 amount) public {
+        require (amount != 0, "Deb0x: your amount is 0");
+        require(balance[msg.sender] - amount > 0, "Deb0x: insufficient balance");
+        
+        deboxERC20.approve(address(this), amount);
+        deboxERC20.transferFrom(address(this), msg.sender, amount);
+
+        balance[msg.sender] -= amount;
     }
 
      function sendViaCall(address payable _to, uint256 _amount) private {
--- contracts/Deb0xERC20.sol
+++ contracts/Deb0xERC20.sol
@@ -4,9 +4,12 @@ pragma solidity ^0.8.11;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Deb0xERC20 is ERC20 {
-    constructor() ERC20("Deb0x", "DBX") {}
+    uint256 public constant TOTAL_SUPPLY = 1000000 * (10 ** 18);
 
-    function mint(address beneficiary, uint256 mintAmount) external {
-        _mint(beneficiary, mintAmount);
+    address public deb0x = 0x17D32D28280e6EfBDC93840C5742101851dCF853;
+
+    constructor() ERC20("Deb0x", "DBX") {
+        _mint(deb0x, TOTAL_SUPPLY);
+
     }
 }
