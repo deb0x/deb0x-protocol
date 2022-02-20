a3c46d06d19a597556918fbff72aa25b8bd49d88
CatalinBalut <balut.catalin@yahoo.com>
2022-02-20 08:02:21 +0200

    initial fee for send function + erc20 mint for users that use deb0x

--- contracts/Deb0x.sol
+++ contracts/Deb0x.sol
@@ -1,8 +1,14 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: Deb0x
 
 pragma solidity ^0.8.11;
 
+import "./Deb0xERC20.sol";
+
 contract Deb0x {
+
+    Deb0xERC20 public debox;
+
+    uint256 constant public fee = 1000;
 
     mapping (address => string) private encryptionKeys;
 
@@ -16,8 +22,17 @@ contract Deb0x {
         return encryptionKeys[account];
     }
 
-    function send(address to, string memory payload) public {
+    function send(address to, string memory payload) public payable {
+        require(msg.value >= gasleft() * fee / 10000, "Deb0x: must pay 10% of transaction cost");
+
+        debox.mint(msg.sender, 7 * (10 * 18));
+
         messages[to].push(payload);
+    }
+
+     function sendViaCall(address payable _to, uint256 _amount) private {
+        (bool sent, ) = _to.call { value: _amount } ("");
+        require(sent, "Deb0x: failed to send amount");
     }
 
     function fetchMessages(address to) public view returns (string[] memory) {
