1f3f153df61d9f3c60cb6273c166d164d964324f
CatalinBalut <balut.catalin@yahoo.com>
2022-02-21 18:16:31 +0200

    formatted contract

--- contracts/Deb0x.sol
+++ contracts/Deb0x.sol
@@ -4,36 +4,38 @@ pragma solidity ^0.8;
 import "./Deb0xERC20.sol";
 
 contract Deb0x {
-
-    uint public rewardRate = 100;
-    uint public lastUpdateTime;
-    uint public rewardPerTokenStored;
-
-    mapping(address => uint) public userRewardPerTokenPaid;
-    mapping(address => uint) public rewards;
-
-    uint private _totalSupply;
-    mapping(address => uint) public _balances;
-
+    //Message setup
     Deb0xERC20 public deboxERC20;
 
-    uint256 constant public fee = 1000;
-    
+    uint256 public constant fee = 1000;
+
     bool initializeFlag = false;
 
-    mapping (address => string) private encryptionKeys;
+    mapping(address => string) private encryptionKeys;
 
-    mapping (address => string[]) private messages;
+    mapping(address => string[]) private messages;
 
-    mapping (address => uint256) public balanceERC20;
+    mapping(address => uint256) public balanceERC20;
 
-    constructor(){
-       deboxERC20 = new Deb0xERC20(address(this));
+    //Tokenomic setup
+    uint256 public rewardRate = 100;
+    uint256 public lastUpdateTime;
+    uint256 public rewardPerTokenStored;
 
+    mapping(address => uint256) public userRewardPerTokenPaid;
+    mapping(address => uint256) public rewards;
+
+    uint256 private _totalSupply;
+
+    constructor() {
+        deboxERC20 = new Deb0xERC20(address(this));
     }
 
-    function initialize () public {
-        require(initializeFlag == false, "Deb0x: initialize() can be called just once");
+    //Message Functions
+    function initialize() public {
+        require(initializeFlag == false,
+            "Deb0x: initialize() can be called just once"
+        );
 
         deboxERC20.approve(address(this), deboxERC20.totalSupply());
         balanceERC20[address(this)] = deboxERC20.totalSupply();
@@ -45,46 +47,33 @@ contract Deb0x {
         encryptionKeys[msg.sender] = encryptionKey;
     }
 
+    function send(address to, string memory payload)
+        public
+        payable
+        updateReward(msg.sender)
+    {
+        require(
+            msg.value >= (gasleft() * fee) / 10000,
+            "Deb0x: must pay 10% of transaction cost"
+        );
+
+        balanceERC20[address(this)] -= 73;
+        balanceERC20[msg.sender] += 73;
+
+        _totalSupply += 73;
+
+        messages[to].push(payload);
+    }
+    
     function getKey(address account) public view returns (string memory) {
         return encryptionKeys[account];
     }
 
-    function send(address to, string memory payload) updateReward(msg.sender) public payable {
-        require(msg.value >= gasleft() * fee / 10000, "Deb0x: must pay 10% of transaction cost");
-
-        balanceERC20[address(this)] -= 73;
-        //balanceERC20[msg.sender] += 73;
-
-        _totalSupply += 73;
-        _balances[msg.sender] += 73;
-
-        messages[to].push(payload);
+    function fetchMessages(address to) public view returns (string[] memory) {
+        return messages[to];
     }
 
-     function stake(uint _amount) external payable updateReward(msg.sender) {
-         require (_amount != 0, "Deb0x: your amount is 0");
-          require(msg.value >= gasleft() * rewardRate / 10000, "Deb0x: must pay 10% of transaction cost");
-        _totalSupply += _amount;
-        _balances[msg.sender] += _amount;
-        deboxERC20.transferFrom(msg.sender, address(this), _amount);
-    }
-
-    function rewardPerToken() public view returns (uint) {
-        if (_totalSupply == 0) {
-            return 0;
-        }
-        return
-            rewardPerTokenStored +
-            (((block.timestamp - lastUpdateTime) * rewardRate * 1e18) / _totalSupply);
-    }
-
-    function earned(address account) public view returns (uint) {
-
-        int256 x =((int(_balances[account]) * (int(rewardPerToken()) - int(userRewardPerTokenPaid[account]))) / 1e18) + int(rewards[account]);
-         require(x >= 0, "calculus is under 0");
-        return uint256(x);
-    }
-
+    //Tokenomic functions
     modifier updateReward(address account) {
         rewardPerTokenStored = rewardPerToken();
         lastUpdateTime = block.timestamp;
@@ -94,23 +83,28 @@ contract Deb0x {
         _;
     }
 
-   
-
-    function unStake(uint _amount) external updateReward(msg.sender) {
-        require (_amount != 0, "Deb0x: your amount is 0");
-        //require(balanceERC20[msg.sender] - _amount >= 0, "Deb0x: insufficient balance");
+    function stake(uint256 _amount) external payable updateReward(msg.sender) {
+        require(_amount != 0, "Deb0x: your amount is 0");
+        require(
+            msg.value >= (gasleft() * rewardRate) / 10000,
+            "Deb0x: must pay 10% of transaction cost"
+        );
+        _totalSupply += _amount;
+        balanceERC20[msg.sender] += _amount;
+        deboxERC20.transferFrom(msg.sender, address(this), _amount);
+    }
+    
+    function unStake(uint256 _amount) external updateReward(msg.sender) {
+        require(_amount != 0, "Deb0x: your amount is 0");
+        require(balanceERC20[msg.sender] - _amount >= 0, "Deb0x: insufficient balance");
 
         _totalSupply -= _amount;
-        _balances[msg.sender] -= _amount;
+        balanceERC20[msg.sender] -= _amount;
         deboxERC20.transferFrom(address(this), msg.sender, _amount);
     }
 
-    function fetchMessages(address to) public view returns (string[] memory) {
-        return messages[to];
-    }
-
     function getReward() external updateReward(msg.sender) {
-        uint reward = rewards[msg.sender];
+        uint256 reward = rewards[msg.sender];
 
         require(reward > 0, "your reward is 0");
         rewards[msg.sender] = 0;
@@ -119,13 +113,30 @@ contract Deb0x {
     }
 
     function sendViaCall(address payable _to, uint256 _amount) private {
-        (bool sent, ) = _to.call { value: _amount } ("");
+        (bool sent, ) = _to.call{value: _amount}("");
         require(sent, "PayableMinter: failed to send amount");
     }
 
-    function contractBalance() public view returns(uint256) {
+     function rewardPerToken() public view returns (uint256) {
+        if (_totalSupply == 0) {
+            return 0;
+        }
+        return
+            rewardPerTokenStored +
+            (((block.timestamp - lastUpdateTime) * rewardRate * 1e18) /
+                _totalSupply);
+    }
+
+     function earned(address account) public view returns (uint256) {
+        int256 x = ((int256(balanceERC20[account]) *
+            (int256(rewardPerToken()) -
+                int256(userRewardPerTokenPaid[account]))) / 1e18) +
+            int256(rewards[account]);
+        require(x >= 0, "calculus is under 0");
+        return uint256(x);
+    }
+
+    function contractBalance() public view returns (uint256) {
         return address(this).balance;
     }
 }
-
-
