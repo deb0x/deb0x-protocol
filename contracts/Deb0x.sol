6867b3e4a5d3a30dddf8b0a215cc62c7653709de
Alexhandru <alextitieni@yahoo.com>
2022-06-28 16:48:13 +0300

    Fee distribution in progress

--- contracts/Deb0x.sol
+++ contracts/Deb0x.sol
@@ -3,9 +3,16 @@ pragma solidity ^0.8.11;
 
 import "./Deb0xCore.sol";
 import "./DBX.sol";
+import "hardhat/console.sol";
 
 contract Deb0x is Deb0xCore {
     //Message setup
+    struct Stake {
+        uint256 stakeCycle;
+
+        uint256 stakeAmount;
+    }
+
     DBX public dbx;
     uint16 public constant MAIL_FEE = 1000;
     uint256 immutable i_initialTimestamp;
@@ -20,20 +27,29 @@ contract Deb0x is Deb0xCore {
     mapping(address => uint256) lastActiveCycle;
     mapping(address => uint256) frontEndLastCycleUpdate;
     mapping(address => uint256) addressRewards;
+    mapping(address => uint256) public addressAccruedFees;
     mapping(address => uint256) frontendRewards;
     mapping(uint256 => uint256) public rewardPerCycle;
-
-    uint256 public totalSupply;
+    mapping(uint256 => uint256) public summedCycleStakes;
+    mapping(address => uint256) lastFeeUpdateCycle;
+    mapping(uint256 => uint256) cycleAccruedFees;
+    mapping(uint256 => uint256) public cycleFeesPerStake;
+    mapping(uint256 => uint256) public cycleFeesPerStakeSummed;
+    mapping(address => mapping(uint256 => uint256)) userStakeCycle;
+    mapping(address => uint256) userTotalStake;
+    mapping(address => uint256) userFirstStake;
+    mapping(address => uint256) userSecondStake;
 
     constructor() {
         dbx = new DBX();
         i_initialTimestamp = block.timestamp;
         i_periodDuration = 1 days;
         currentCycleReward = 100 * 1e18;
+        summedCycleStakes[0] = 100 * 1e18;
         rewardPerCycle[0] = 100 * 1e18;
     }
 
-    modifier notify(address account) {
+    modifier setUpNewCycle() {
         uint256 currentCycle = getCurrentCycle();
         if(rewardPerCycle[currentCycle] == 0) {
             lastCycleReward = currentCycleReward;
@@ -41,6 +57,37 @@ contract Deb0x is Deb0xCore {
             currentCycleReward = calculatedCycleReward;
             rewardPerCycle[currentCycle] = calculatedCycleReward;
         }
+        _;
+    }
+
+    modifier notify(address account) {
+        uint256 currentCycle = getCurrentCycle();
+        if(summedCycleStakes[currentCycle] == 0) {
+            uint256 calculatedCycleReward = calculateCycleReward();
+            summedCycleStakes[currentCycle] = summedCycleStakes[currentCycle - 1] + calculatedCycleReward;
+            uint256 feePerStake = cycleAccruedFees[currentCycle - 1] * 1e18 / summedCycleStakes[currentCycle - 1];
+            cycleFeesPerStakeSummed[currentCycle] = cycleFeesPerStakeSummed[currentCycle - 1] + feePerStake;
+        }
+
+        if(userFirstStake[account] != 0) {
+            if(userFirstStake[account] - currentCycle > 1){
+                addressRewards[account] += userStakeCycle[account][userFirstStake[account]];
+                userStakeCycle[account][userFirstStake[account]] = 0;
+                userFirstStake[account] = 0;
+
+                if(userSecondStake[account] != 0) {
+                    if(userSecondStake[account] - currentCycle > 1) {
+                        addressRewards[account] += userStakeCycle[account][userSecondStake[account]];
+                        userStakeCycle[account][userSecondStake[account]] = 0;
+                        userSecondStake[account] = 0;
+                        } else {
+                            userFirstStake[account] = userSecondStake[account];
+                            userSecondStake[account] = 0;
+                        }
+                    }
+            }
+        }
+
         if(currentCycle > lastActiveCycle[account] && cycleTotalMessages[lastActiveCycle[account]] != 0) {
             uint256 lastCycleUserReward = userCycleMessages[account] * rewardPerCycle[lastActiveCycle[account]] / cycleTotalMessages[lastActiveCycle[account]];
             if(cycleTotalMessages[lastActiveCycle[account]] != 0) {
@@ -53,6 +100,13 @@ contract Deb0x is Deb0xCore {
                 userCycleFeePercent[account] = 0;
             }
             userCycleMessages[account] = 0;
+            
+        }
+        
+        if(currentCycle > lastFeeUpdateCycle[account]){
+            addressAccruedFees[account] = addressAccruedFees[account] + ((addressRewards[account] 
+                * (cycleFeesPerStakeSummed[currentCycle] - cycleFeesPerStakeSummed[lastFeeUpdateCycle[account]]))) / 1e18;
+            lastFeeUpdateCycle[account] = currentCycle;
         }
         _;
     }
@@ -71,13 +125,15 @@ contract Deb0x is Deb0xCore {
 
     function send(address[] memory to, string[] memory payload, address feeReceiver, uint256 msgFee)
         public
+        payable
+        setUpNewCycle
         notify(msg.sender)
     {
         uint256 currentCycle = getCurrentCycle();
 
         updateFrontEndReward(feeReceiver, currentCycle);
 
-        //uint256 startGas = gasleft();
+        uint256 startGas = gasleft();
 
         userCycleMessages[msg.sender]++;
         cycleTotalMessages[currentCycle]++;
@@ -89,22 +145,22 @@ contract Deb0x is Deb0xCore {
         }
 
         super.send(to, payload);
-
-        // messagesSent[msg.sender] += 1;
-        // totalSupply += 1;
-    
-        //uint256 gasUsed = startGas - gasleft();
-        //require(
-            //msg.value >=
-                //((gasUsed+ 21000 + 50000) * tx.gasprice  * fee) / 10000,
-            //"Deb0x: must pay 10% of transaction cost"
-        //);
+        cycleAccruedFees[currentCycle] += msg.value;
+        uint256 gasUsed = startGas - gasleft();
+        
+        require(
+            msg.value >=
+                ((gasUsed+ 21000 + 50000) * tx.gasprice  * MAIL_FEE) / 10000,
+            "Deb0x: must pay 10% of transaction cost"
+        );
     }
 
-    function claimRewards() public notify(msg.sender) {
+    function claimRewards() public setUpNewCycle notify(msg.sender) {
+        uint256 currentCycle = getCurrentCycle();
         uint256 reward = addressRewards[msg.sender];
         require(reward > 0, "Deb0x: You do not have rewards");
         addressRewards[msg.sender] = 0;
+        summedCycleStakes[currentCycle] = summedCycleStakes[currentCycle] - reward;
         dbx.mintReward(msg.sender, reward);
     }
 
@@ -118,30 +174,39 @@ contract Deb0x is Deb0xCore {
         dbx.mintReward(msg.sender, reward);
     }
 
-    // function stakeERC20(uint256 _amount)
-    //     external
-    //     payable
-    //     updateReward(msg.sender)
-    // {
-    //     require(_amount != 0, "Deb0x: your amount is 0");
+    function claimFees() public setUpNewCycle notify(msg.sender){
+        uint256 fees = addressAccruedFees[msg.sender];
+        require(fees > 0, "Deb0x: You do not have accrued fees");
+        addressAccruedFees[msg.sender] = 0;
+        sendViaCall(payable(msg.sender), fees);
+    }
 
-    //     totalSupply += _amount;
-    //     balanceERC20[msg.sender] += _amount;
+    function stakeDBX(uint256 _amount)
+        external
+        setUpNewCycle
+        notify(msg.sender)
+    {
+        require(_amount != 0, "Deb0x: your amount is 0");
+        uint256 currentCycle = getCurrentCycle();
+        summedCycleStakes[currentCycle] += _amount;
 
-    //     deboxERC20.transferFrom(msg.sender, address(this), _amount);
-    // }
+        if(userFirstStake[msg.sender] == 0) {
+            userFirstStake[msg.sender] = currentCycle;
+            
+        } else if(userSecondStake[msg.sender] == 0) {
+            userSecondStake[msg.sender] = currentCycle;
+        }
+        userStakeCycle[msg.sender][currentCycle] += _amount;
+        userTotalStake[msg.sender] += _amount;
 
-    // function unStakeERC20(uint256 _amount) external updateReward(msg.sender) {
-    //     require(_amount != 0, "Deb0x: your amount is 0");
-    //     require(
-    //         balanceERC20[msg.sender] - _amount >= 0,
-    //         "Deb0x: insufficient balance"
-    //     );
+        dbx.transferFrom(msg.sender, address(this), _amount);
+    }
 
-    //     totalSupply -= _amount;
-    //     balanceERC20[msg.sender] -= _amount;
-    //     deboxERC20.transferFrom(address(this), msg.sender, _amount);
-    // }
+    function sendViaCall(address payable _to, uint256 _amount) private {
+        (bool sent, ) = _to.call{value: _amount}("");
+        console.log(_amount);
+        require(sent, "Deb0x: failed to send amount");
+    }
 
     function contractBalance() public view returns (uint256) {
         return address(this).balance;
