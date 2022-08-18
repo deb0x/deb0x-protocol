9581c1469b066ba07c601d1c770fa5f439a751e7
corchessergiu <corchessergiu194@gmail.com>
2022-08-18 19:12:45 +0300

    rework script

--- scritps/reward_script.js
+++ scritps/reward_script.js
@@ -1,21 +1,23 @@
 const fs = require('fs')
 const { BigNumber } = require("ethers");
 async function calculateRewardDistribution() {
-    let reward = BigNumber.from("100000000000000000000");
-    let lastReward = 0;
+    let lastReward = BigNumber.from("100000000000000000000");
+    let actualReward = 0;
     let iteration = 0;
-    while (reward > 0) {
-        lastReward = (reward.mul(BigNumber.from("10000000000000000000000"))).div(BigNumber.from("10019000000000000000000"));
-        reward = lastReward;
-        console.log(ethers.utils.formatEther(reward))
-        fs.appendFile('result.txt', ethers.utils.formatEther(reward) + '\n', err => {
+    let totalRewad = BigNumber.from("10000000000000000000000");
+    while (lastReward > 0) {
+        actualReward = (lastReward.mul(BigNumber.from("10000"))).div(BigNumber.from("10019"));
+        lastReward = actualReward;
+        totalRewad = BigNumber.from(totalRewad).add(BigNumber.from(lastReward))
+        console.log(ethers.utils.formatEther(lastReward))
+        fs.appendFile('result.txt', ethers.utils.formatEther(lastReward) + '\n', err => {
             if (err) {
                 console.error(err);
             }
         });
         iteration++;
     }
-    console.log("Numar de iteratii ->>> " + iteration)
+    console.log("Numar de iteratii ->>> " + iteration + " " + ethers.utils.formatEther(totalRewad))
 }
 
 calculateRewardDistribution()
