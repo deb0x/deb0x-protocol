caf5aeaf6afd3a790eafce5efd1bbc1bfda183f2
Alex Males <alexandru.males@gmail.com>
2022-09-29 15:37:44 +0300

    Test for reward (no fees) and NumUtils

--- test/Deb0x/test_claimRewardsNoFees.js
+++ test/Deb0x/test_claimRewardsNoFees.js
@@ -1,6 +1,7 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
-const { abi } = require("../../artifacts/contracts/Deb0xERC20.sol/Deb0xERC20.json")
+const { NumUtils } = require("../utils/NumUtils.ts");
+const { abi } = require("../../artifacts/contracts/Deb0xERC20.sol/Deb0xERC20.json");
 
 describe("Test contract", async function () {
   let rewardedAlice, rewardedBob, rewardedCarol, dbxERC20;
@@ -25,7 +26,8 @@ describe("Test contract", async function () {
     await hre.ethers.provider.send("evm_mine")
 
     try {
-      const claimedRewards = await rewardedAlice.claimRewards()
+      await rewardedAlice.claimRewards();
+      expect.fail("An exception was expected");      
     } catch(error) {
       expect(error.message).to.equal("VM Exception while processing transaction: " +
                 "reverted with reason string 'Deb0x: You do not have rewards'");
@@ -33,23 +35,18 @@ describe("Test contract", async function () {
   });
 
   it("Should claim no rewards for sending a message in the current cycle", async () => {
-
     await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"], ethers.constants.AddressZero, 0, 0, { value: ethers.utils.parseEther("1") })
 
-    
-
     try {
-      const claimedRewards = await rewardedAlice.claimRewards()
+      await rewardedAlice.claimRewards()
+      expect.fail("An exception was expected");      
     } catch(error) {
       expect(error.message).to.equal("VM Exception while processing transaction: " +
                 "reverted with reason string 'Deb0x: You do not have rewards'");
     }
-    console.log(await dbxERC20.balanceOf(alice.address))
   });
 
-
   it("Should claim share of rewards after sending a message in the previous day", async () => {
-    //console.log(await rewardedAlice.getCurrentCycleReward())
     await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"], ethers.constants.AddressZero, 0, 0, { value: ethers.utils.parseEther("1") })
     await rewardedBob["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"], ethers.constants.AddressZero, 0, 0, { value: ethers.utils.parseEther("1") })
     await rewardedBob["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"], ethers.constants.AddressZero, 0, 0, { value: ethers.utils.parseEther("1") })
@@ -57,8 +54,12 @@ describe("Test contract", async function () {
     await hre.ethers.provider.send("evm_increaseTime", [60 * 60 * 24])
     await hre.ethers.provider.send("evm_mine")
 
-    const claimedRewards = await rewardedAlice.claimRewards()
-    console.log(await dbxERC20.balanceOf(alice.address))
+    await rewardedAlice.claimRewards();
+    const balance = await dbxERC20.balanceOf(alice.address);
+
+    const expected = NumUtils.ether(100).div(3);
+
+    expect(balance).to.equal(expected);
   });
   
   it("Should be able to claim previous cycle rewards and not reset current messages counter", async () => {
@@ -70,17 +71,21 @@ describe("Test contract", async function () {
     await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"], ethers.constants.AddressZero, 0, 0, { value: ethers.utils.parseEther("1") })    
 
     await rewardedAlice.claimRewards()
-    console.log(await dbxERC20.balanceOf(alice.address))
+    expect(await dbxERC20.balanceOf(alice.address)).to.equal(NumUtils.day1());
 
     await hre.ethers.provider.send("evm_increaseTime", [60 * 60 * 24])
     await hre.ethers.provider.send("evm_mine")
 
     await rewardedAlice.claimRewards()
-    console.log(await dbxERC20.balanceOf(alice.address))
+
+    const day1 = NumUtils.day1();
+    const day2 = NumUtils.day2();
+    const expected = day1.add(day2);
+
+    expect(await dbxERC20.balanceOf(alice.address)).to.equal(expected);
   });
 
   it("Should be able to claim previous 2 cycles rewards", async () => {
-    //console.log(await rewardedAlice.getCurrentCycle())
     await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"], ethers.constants.AddressZero, 0, 0, { value: ethers.utils.parseEther("1") })
     await hre.ethers.provider.send("evm_increaseTime", [60 * 60 * 24])
     await hre.ethers.provider.send("evm_mine")
@@ -91,7 +96,12 @@ describe("Test contract", async function () {
     await hre.ethers.provider.send("evm_mine")
 
     await rewardedAlice.claimRewards()
-    console.log(await dbxERC20.balanceOf(alice.address))
+
+    const day1 = NumUtils.day1();
+    const day2 = NumUtils.day2();
+    const expected = day1.add(day2);
+
+    expect(await dbxERC20.balanceOf(alice.address)).to.equal(expected);
   });
 
   it("Should claim share of rewards after sending a message in the previous day", async () => {
@@ -116,15 +126,41 @@ describe("Test contract", async function () {
     await hre.ethers.provider.send("evm_increaseTime", [60 * 60 * 24])
     await hre.ethers.provider.send("evm_mine")
 
-    for(let i = 0; i < 5; i++) {
-      console.log(await rewardedAlice.rewardPerCycle(i))
-    }
-
     await rewardedAlice.claimRewards()
     await rewardedBob.claimRewards()
     await rewardedCarol.claimRewards()
-    console.log("Alice " + await dbxERC20.balanceOf(alice.address))
-    console.log("Bob " + await dbxERC20.balanceOf(bob.address))
-    console.log("Carol " + await dbxERC20.balanceOf(carol.address))
+    
+    // Alice  gets entire day2 reward and 2/3 of day3   
+    const aliceExpected = NumUtils.day(2).add(
+      NumUtils.day(3).mul(2).div(3)
+    );
+    expect(await dbxERC20.balanceOf(alice.address)).to.equal(aliceExpected);
+
+    // Bob    gets entire 1/3 of day3
+    const bobExpected = NumUtils.day(3).div(3);
+    expect(await dbxERC20.balanceOf(bob.address)).to.equal(bobExpected);
+
+    // Carol  gets entire day4 reward
+    const carolExpected = NumUtils.day(4);
+    expect(await dbxERC20.balanceOf(carol.address)).to.equal(carolExpected);
+  });
+
+  it("On day 6, Should claim day 2 rewards after sending one message in day 1 and one message in day 5", async () => {
+    
+    await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"], ethers.constants.AddressZero, 0, 0, { value: ethers.utils.parseEther("1") })
+
+    await hre.ethers.provider.send("evm_increaseTime", [60 * 60 * 24 * 4])
+    await hre.ethers.provider.send("evm_mine")
+
+    await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"], ethers.constants.AddressZero, 0, 0, { value: ethers.utils.parseEther("1") })
+
+    await hre.ethers.provider.send("evm_increaseTime", [60 * 60 * 24])
+    await hre.ethers.provider.send("evm_mine")
+
+    await rewardedAlice.claimRewards()
+    
+    // Alice  gets day1 and day2 rewards   
+    const aliceExpected = NumUtils.day1().add(NumUtils.day2());
+    expect(await dbxERC20.balanceOf(alice.address)).to.equal(aliceExpected);
   });
 });
