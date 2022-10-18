15ab9a2c84e6773b8d36604c37cdda90fc2ea1c5
corchessergiu <corchessergiu194@gmail.com>
2022-10-18 16:08:34 +0300

    rework tests

--- test/Send/test_send2.js
+++ test/Send/test_send2.js
@@ -17,8 +17,6 @@ describe("Test send messages and fetch functions", async function() {
         const dbxAddress = await rewardedAlice.dbx()
         dbxERC20 = new ethers.Contract(dbxAddress, abi, hre.ethers.provider)
 
-        // dbxCore = new ethers.Contract(dbx) 
-
         rewardedBob = rewardedAlice.connect(bob)
         rewardedCarol = rewardedAlice.connect(carol)
         frontend = rewardedAlice.connect(feeReceiver)
@@ -29,58 +27,111 @@ describe("Test send messages and fetch functions", async function() {
         aliceBalance = await hre.ethers.provider.getBalance(alice.address)
         let curCycle = parseInt((await rewardedAlice.getCurrentCycle()).toString())
 
-        await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
+        let sendFirstMessage = await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
             feeReceiver.address, 200, 0, { value: ethers.utils.parseEther("2") })
+        let receiptFirstMessage = await sendFirstMessage.wait();
 
-        await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
+        let sendSecondMessage = await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
             feeReceiver.address, 100, 0, { value: ethers.utils.parseEther("2") })
+        let receiptSecondMessage = await sendSecondMessage.wait();
 
-        await rewardedBob["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
+        let sendThirdMessage = await rewardedBob["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
             feeReceiver.address, 100, 0, { value: ethers.utils.parseEther("2") })
+        let receiptThirdMessage = await sendThirdMessage.wait();
 
         await hre.ethers.provider.send("evm_increaseTime", [60 * 60 * 24])
         await hre.ethers.provider.send("evm_mine")
 
-        let messagesAlice = await rewardedAlice.fetchSentMessages(alice.address);
-        expect(messagesAlice.length).to.eq(2);
-    })
+        let totalNumberOfMessagesSentByAlice = 0;
+        for (let i = 0; i < receiptFirstMessage.events.length - 1; i++) {
+            if (receiptFirstMessage.events[i].event === 'Sent' && receiptFirstMessage.events[i].args.from === alice.address) {
+                totalNumberOfMessagesSentByAlice++;
+            }
+        }
+        for (let i = 0; i < receiptSecondMessage.events.length - 1; i++) {
+            if (receiptSecondMessage.events[i].event === 'Sent' && receiptSecondMessage.events[i].args.from === alice.address) {
+                totalNumberOfMessagesSentByAlice++;
+            }
+        }
+        for (let i = 0; i < receiptThirdMessage.events.length - 1; i++) {
+            if (receiptThirdMessage.events[i].event === 'Sent' && receiptThirdMessage.events[i].args.from === alice.address) {
+                totalNumberOfMessagesSentByAlice++;
+            }
+        }
 
+        expect(totalNumberOfMessagesSentByAlice).to.eq(2);
+    })
 
     it("should test sending some messages  in different cycles", async() => {
 
         aliceBalance = await hre.ethers.provider.getBalance(alice.address)
         let curCycle = parseInt((await rewardedAlice.getCurrentCycle()).toString())
 
-        await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
+        let sendFirstMessage = await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
             feeReceiver.address, 200, 0, { value: ethers.utils.parseEther("2") })
+        let receiptFirstMessage = await sendFirstMessage.wait();
 
-        await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
+        let sendSecondMessage = await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
             feeReceiver.address, 100, 0, { value: ethers.utils.parseEther("2") })
+        let receiptSecondMessage = await sendSecondMessage.wait();
 
-        await rewardedBob["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
+        let sendThirdMessage = await rewardedBob["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
             feeReceiver.address, 100, 0, { value: ethers.utils.parseEther("2") })
-
+        let receiptThirdMessage = await sendThirdMessage.wait();
 
         await hre.ethers.provider.send("evm_increaseTime", [60 * 60 * 24])
         await hre.ethers.provider.send("evm_mine")
 
-        await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
+        let sendFourthMessage = await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
             feeReceiver.address, 200, 0, { value: ethers.utils.parseEther("2") })
+        let receiptForthMessage = await sendFourthMessage.wait();
 
-        await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
+        let sendFifthMessage = await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
             feeReceiver.address, 100, 0, { value: ethers.utils.parseEther("2") })
+        let receiptFifthMessage = await sendFifthMessage.wait();
 
         await hre.ethers.provider.send("evm_increaseTime", [60 * 60 * 24])
         await hre.ethers.provider.send("evm_mine")
         await hre.ethers.provider.send("evm_increaseTime", [60 * 60 * 24])
         await hre.ethers.provider.send("evm_mine")
 
-
-        await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
+        let sendSixthMessage = await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
             feeReceiver.address, 100, 0, { value: ethers.utils.parseEther("2") })
+        let receiptSixthMessage = await sendSixthMessage.wait();
 
-        let messagesAlice = await rewardedAlice.fetchSentMessages(alice.address);
-        expect(messagesAlice.length).to.eq(5);
+        let totalNumberOfMessagesSentByAlice = 0;
+        for (let i = 0; i < receiptFirstMessage.events.length - 1; i++) {
+            if (receiptFirstMessage.events[i].event === 'Sent' && receiptFirstMessage.events[i].args.from === alice.address) {
+                totalNumberOfMessagesSentByAlice++;
+            }
+        }
+        for (let i = 0; i < receiptSecondMessage.events.length - 1; i++) {
+            if (receiptSecondMessage.events[i].event === 'Sent' && receiptSecondMessage.events[i].args.from === alice.address) {
+                totalNumberOfMessagesSentByAlice++;
+            }
+        }
+        for (let i = 0; i < receiptThirdMessage.events.length - 1; i++) {
+            if (receiptThirdMessage.events[i].event === 'Sent' && receiptThirdMessage.events[i].args.from === alice.address) {
+                totalNumberOfMessagesSentByAlice++;
+            }
+        }
+        for (let i = 0; i < receiptForthMessage.events.length - 1; i++) {
+            if (receiptForthMessage.events[i].event === 'Sent' && receiptForthMessage.events[i].args.from === alice.address) {
+                totalNumberOfMessagesSentByAlice++;
+            }
+        }
+        for (let i = 0; i < receiptFifthMessage.events.length - 1; i++) {
+            if (receiptFifthMessage.events[i].event === 'Sent' && receiptFifthMessage.events[i].args.from === alice.address) {
+                totalNumberOfMessagesSentByAlice++;
+            }
+        }
+        for (let i = 0; i < receiptSixthMessage.events.length - 1; i++) {
+            if (receiptSixthMessage.events[i].event === 'Sent' && receiptSixthMessage.events[i].args.from === alice.address) {
+                totalNumberOfMessagesSentByAlice++;
+            }
+        }
+
+        expect(totalNumberOfMessagesSentByAlice).to.eq(5);
     })
 
     it("should test sending some messages with feeReceiver = 0x00 address ", async() => {
@@ -88,17 +139,30 @@ describe("Test send messages and fetch functions", async function() {
         aliceBalance = await hre.ethers.provider.getBalance(alice.address)
         let curCycle = parseInt((await rewardedAlice.getCurrentCycle()).toString())
 
-        await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
+        let sendFirstMessage = await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
             "0x0000000000000000000000000000000000000000", 200, 0, { value: ethers.utils.parseEther("2") })
+        let receiptFirstMessage = await sendFirstMessage.wait();
 
-        await rewardedBob["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
+        let sendSecondMessage = await rewardedBob["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
             feeReceiver.address, 100, 0, { value: ethers.utils.parseEther("2") })
+        let receiptSecondMessage = await sendSecondMessage.wait();
 
         await hre.ethers.provider.send("evm_increaseTime", [60 * 60 * 24])
         await hre.ethers.provider.send("evm_mine")
 
-        let messagesAlice = await rewardedAlice.fetchSentMessages(alice.address);
-        expect(messagesAlice.length).to.eq(1);
+        let totalNumberOfMessagesSentByAlice = 0;
+        for (let i = 0; i < receiptFirstMessage.events.length - 1; i++) {
+            if (receiptFirstMessage.events[i].event === 'Sent' && receiptFirstMessage.events[i].args.from === alice.address) {
+                totalNumberOfMessagesSentByAlice++;
+            }
+        }
+        for (let i = 0; i < receiptSecondMessage.events.length - 1; i++) {
+            if (receiptSecondMessage.events[i].event === 'Sent' && receiptSecondMessage.events[i].args.from === alice.address) {
+                totalNumberOfMessagesSentByAlice++;
+            }
+        }
+
+        expect(totalNumberOfMessagesSentByAlice).to.eq(1);
     })
 
     it("should test sending some messages and msgFee + nativeTokenFee have different combinations", async() => {
@@ -106,36 +170,61 @@ describe("Test send messages and fetch functions", async function() {
         aliceBalance = await hre.ethers.provider.getBalance(alice.address)
         let curCycle = parseInt((await rewardedAlice.getCurrentCycle()).toString())
 
-        await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
+        let sendFirstMessage = await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
             feeReceiver.address, 0, 0, { value: ethers.utils.parseEther("2") })
+        let receiptFirstMessage = await sendFirstMessage.wait();
 
-        await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
+        let sendSecondMessage = await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
             feeReceiver.address, 100, 110, { value: ethers.utils.parseEther("2") })
+        let receiptSecondMessage = await sendSecondMessage.wait();
 
-        await rewardedBob["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
+        let sendThirdMessage = await rewardedBob["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
             feeReceiver.address, 0, 100, { value: ethers.utils.parseEther("2") })
+        let receiptThirdMessage = await sendThirdMessage.wait();
 
         await hre.ethers.provider.send("evm_increaseTime", [60 * 60 * 24])
         await hre.ethers.provider.send("evm_mine")
 
-        let messagesAlice = await rewardedAlice.fetchSentMessages(alice.address);
-        expect(messagesAlice.length).to.eq(2);
+        let totalNumberOfMessagesSentByAlice = 0;
+        for (let i = 0; i < receiptFirstMessage.events.length - 1; i++) {
+            if (receiptFirstMessage.events[i].event === 'Sent' && receiptFirstMessage.events[i].args.from === alice.address) {
+                totalNumberOfMessagesSentByAlice++;
+            }
+        }
+        for (let i = 0; i < receiptSecondMessage.events.length - 1; i++) {
+            if (receiptSecondMessage.events[i].event === 'Sent' && receiptSecondMessage.events[i].args.from === alice.address) {
+                totalNumberOfMessagesSentByAlice++;
+            }
+        }
+        for (let i = 0; i < receiptThirdMessage.events.length - 1; i++) {
+            if (receiptThirdMessage.events[i].event === 'Sent' && receiptThirdMessage.events[i].args.from === alice.address) {
+                totalNumberOfMessagesSentByAlice++;
+            }
+        }
+
+        expect(totalNumberOfMessagesSentByAlice).to.eq(2);
     })
 
     it("should test sending some messages but we don't send enaugh eth", async() => {
-
         aliceBalance = await hre.ethers.provider.getBalance(alice.address)
         let curCycle = parseInt((await rewardedAlice.getCurrentCycle()).toString())
 
-        await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
+        let sendMessage = await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
             feeReceiver.address, 200, 0, ).then(res => {
             assert.fail("must throw err");
         }).catch(err => {
             expect(err.message).to.contain("Deb0x: must pay 10% of transaction cost")
         })
-
-        let messagesAlice = await rewardedAlice.fetchSentMessages(alice.address);
-        expect(messagesAlice.length).to.eq(0);
+        let totalNumberOfMessagesSentByAlice = 0;
+        if (sendMessage != undefined) {
+            let receiptFirstMessage = await sendMessage.wait();
+            for (let i = 0; i < receiptFirstMessage.events.length - 1; i++) {
+                if (receiptFirstMessage.events[i].event === 'Sent' && receiptFirstMessage.events[i].args.from === alice.address) {
+                    totalNumberOfMessagesSentByAlice++;
+                }
+            }
+        }
+        expect(totalNumberOfMessagesSentByAlice).to.eq(0);
     })
 
     it("should test sending some messages but we don't send enaugh eth V2", async() => {
@@ -143,14 +232,21 @@ describe("Test send messages and fetch functions", async function() {
         aliceBalance = await hre.ethers.provider.getBalance(alice.address)
         let curCycle = parseInt((await rewardedAlice.getCurrentCycle()).toString())
 
-        await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
+        let sendMessage = await rewardedAlice["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"],
             feeReceiver.address, 200, 0, { value: 20 }).then(res => {
             assert.fail("must throw err");
         }).catch(err => {
             expect(err.message).to.contain("Deb0x: must pay 10% of transaction cost")
         })
-
-        let messagesAlice = await rewardedAlice.fetchSentMessages(alice.address);
-        expect(messagesAlice.length).to.eq(0);
+        let totalNumberOfMessagesSentByAlice = 0;
+        if (sendMessage != undefined) {
+            let receiptFirstMessage = await sendMessage.wait();
+            for (let i = 0; i < receiptFirstMessage.events.length - 1; i++) {
+                if (receiptFirstMessage.events[i].event === 'Sent' && receiptFirstMessage.events[i].args.from === alice.address) {
+                    totalNumberOfMessagesSentByAlice++;
+                }
+            }
+        }
+        expect(totalNumberOfMessagesSentByAlice).to.eq(0);
     })
 });
