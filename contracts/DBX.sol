bd03a06b74bdd2d59b7ae7e8e216f68d014c4b68
Alex Males <alex@deb0x.org>
2022-10-20 13:37:56 +0300

    changed DBX name and symbol

--- contracts/DBX.sol
+++ contracts/DBX.sol
@@ -1,17 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.11;
+pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DBX is ERC20 {
+
     address public owner;
 
-    constructor() ERC20("Deb0x", "DBX") {
+    constructor() ERC20("Deb0x Reward Token on Polygon", "pDBX") {
         owner = msg.sender;
     }
 
     function mintReward(address user, uint256 amount) external {
-        require(msg.sender == owner, "Caller is not Deb0x contract.");
+        require(msg.sender == owner, "DBX: caller is not Deb0x contract.");
         _mint(user, amount);
     }
 }
