1626f3faa5c4e48ad41e0266dd395630a4a6e44f
Alex Males <alex@deb0x.org>
2022-10-20 12:33:06 +0300

    sol  version to 0.8.17

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,7 +7,7 @@ const { mnemonic } = require('./.secrets.json')
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-    solidity: "0.8.16",
+    solidity: "0.8.17",
 
     networks: {
         hardhat: {
