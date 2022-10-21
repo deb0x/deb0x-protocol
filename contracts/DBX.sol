881299e85389ed1656d9a325ca43abebd44d03ff
corchessergiu <corchessergiu194@gmail.com>
2022-10-21 12:25:40 +0300

    Add require

--- contracts/DBX.sol
+++ contracts/DBX.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.11;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+import "hardhat/console.sol";
 contract DBX is ERC20 {
     address public owner;
 
@@ -12,6 +13,7 @@ contract DBX is ERC20 {
 
     function mintReward(address user, uint256 amount) external {
         require(msg.sender == owner, "Caller is not Deb0x contract.");
+        require(ERC20(address(this)).totalSupply() < 5100000000000000000000000);
         _mint(user, amount);
     }
 }
