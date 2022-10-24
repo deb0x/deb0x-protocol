4b72f135fdfea1050391a31c3615dd0079e1661b
Alex Males <alex@deb0x.org>
2022-10-24 17:43:08 +0300

    refactored mint total supply test

--- test/Deb0xERC20/test_require.js
+++ test/Deb0xERC20/test_require.js
@@ -4,7 +4,8 @@ const { ethers } = require("hardhat");
 const { abi } = require("../../artifacts/contracts/Deb0xERC20.sol/Deb0xERC20.json")
 const { NumUtils } = require("../utils/NumUtils.ts");
 
-describe("Test ERC20 require", async function() {
+describe("Memory intensive tests: ERC20 supply limit", async function() {
+
     let userReward, user1Reward, user2Reward, user3Reward, frontend, dbxERC20, totalMinted;
     let user1, user2;
     beforeEach("Set enviroment", async() => {
@@ -28,7 +29,27 @@ describe("Test ERC20 require", async function() {
             await user1Reward["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"], ethers.constants.AddressZero, 0, 0, { value: ethers.utils.parseEther("1") })
             await hre.ethers.provider.send("evm_increaseTime", [60 * 60 * 24])
             await hre.ethers.provider.send("evm_mine")
-            await user1Reward.claimRewards();
+            try {
+                await user1Reward.claimRewards();
+            } catch (e) {
+                console.log('Claim rewards failed. Probably last day?');
+            }
+            if (i % 100 == 0) {
+                console.log(`Days past: ${i}`);
+            }
+        }
+
+        console.log(`Last cycle: ${await user1Reward.getCurrentCycle()}`);
+        console.log(`Last reward: ${await user1Reward.calculateCycleReward()}`);
+
+        console.log("Rewards should be ended. Adding 10 more sends");
+        for (let i = 0; i < 10; i++) {
+            await user1Reward["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"], ethers.constants.AddressZero, 0, 0, { value: ethers.utils.parseEther("1") })
+            await hre.ethers.provider.send("evm_increaseTime", [60 * 60 * 24])
+            await hre.ethers.provider.send("evm_mine")
+            if (i % 100 == 0) {
+                console.log(`Days past: ${i}`);
+            }
         }
 
         await hre.ethers.provider.send("evm_increaseTime", [60 * 60 * 24])
@@ -40,7 +61,7 @@ describe("Test ERC20 require", async function() {
     });
 
     //This test can be use to test require in Deb0xERC20 contract. Require condition must be modified with a value lower than total supply
-    it(`Should test require`, async() => {
+    it.ignore(`Should test require`, async() => {
         try {
             for (let i = 0; i < 15000; i++) {
                 await user1Reward["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"], ethers.constants.AddressZero, 0, 0, { value: ethers.utils.parseEther("1") })
@@ -52,7 +73,4 @@ describe("Test ERC20 require", async function() {
             expect(error.message).to.include("DBX: max supply already minted");
         }
     });
-
-
-
 });
