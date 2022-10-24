f8f9b57e6e61d7af9c81f8099169de3ec8621b08
corchessergiu <corchessergiu194@gmail.com>
2022-10-24 16:33:38 +0300

    add partial test

--- test/Deb0x/test_multipleRecipients.js
+++ test/Deb0x/test_multipleRecipients.js
@@ -9,7 +9,7 @@ describe("Test reward distribution for multiple recipients", async function() {
     let userReward, user1Reward, user2Reward, user3Reward, frontend, dbxERC20;
     let user1, user2;
     beforeEach("Set enviroment", async() => {
-        [user1, user2, user3, messageReceiver, feeReceiver] = await ethers.getSigners();
+        [user1, user2, user3, user4, user5, user6, user7, user8, user9, user10, messageReceiver, feeReceiver] = await ethers.getSigners();
 
         const Deb0x = await ethers.getContractFactory("Deb0x");
         userReward = await Deb0x.deploy(ethers.constants.AddressZero);
@@ -40,7 +40,7 @@ describe("Test reward distribution for multiple recipients", async function() {
         expect(firstCycleRewardPerUser).to.equal(balanceForUser2)
     });
 
-    it.only(`Test multiple recipients, multiple cycles`, async() => {
+    it(`Test multiple recipients, multiple cycles`, async() => {
         await user1Reward["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address, user2.address], ["ipfs://", "ipfs://"], ethers.constants.AddressZero, 0, 0, { value: ethers.utils.parseEther("1") })
         await user2Reward["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"], ethers.constants.AddressZero, 0, 0, { value: ethers.utils.parseEther("1") })
         await hre.ethers.provider.send("evm_increaseTime", [60 * 60 * 24])
@@ -125,5 +125,22 @@ describe("Test reward distribution for multiple recipients", async function() {
         let balanceForUser3Cycle5 = await dbxERC20.balanceOf(user3.address);
         expect(BigNumber.from(fifthCycleRewardPerUser).mul(BigNumber.from(2))).to.equal(BigNumber.from(balanceForUser3Cycle5).sub(BigNumber.from(balanceForUser3Cycle4)));
     });
+
+    it.only(`Test multiple recipients simple case`, async() => {
+        await user1Reward["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address, user2.address, user3.address, user4.address, user5.address, user6.address, user7.address, user8.address, user9.address, user10.address], ["ipfs://", "ipfs://", "ipfs://", "ipfs://", "ipfs://", "ipfs://", "ipfs://", "ipfs://", "ipfs://", "ipfs"], ethers.constants.AddressZero, 0, 0, { value: ethers.utils.parseEther("1") })
+        await user2Reward["send(address[],string[],address,uint256,uint256)"]([messageReceiver.address], ["ipfs://"], ethers.constants.AddressZero, 0, 0, { value: ethers.utils.parseEther("1") })
+        await hre.ethers.provider.send("evm_increaseTime", [60 * 60 * 24])
+        await hre.ethers.provider.send("evm_mine")
+
+        let firstCycleRewardPerUser = NumUtils.day(1).div(11);
+        await user1Reward.claimRewards();
+        let balanceForUser1 = await dbxERC20.balanceOf(user1.address);
+        expect(BigNumber.from(firstCycleRewardPerUser).mul(BigNumber.from(10))).to.equal(balanceForUser1)
+
+        await user2Reward.claimRewards();
+        let balanceForUser2 = await dbxERC20.balanceOf(user2.address);
+        expect(firstCycleRewardPerUser).to.equal(balanceForUser2)
+    });
+
 
 })
