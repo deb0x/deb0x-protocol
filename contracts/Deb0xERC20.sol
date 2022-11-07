ab8d4031960649dd0408a1d35b27b6bac93e57d6
Alexhandru <alextitieni@yahoo.com>
2022-11-07 16:03:24 +0200

    ERC20 with permit + test

--- contracts/Deb0xERC20.sol
+++ contracts/Deb0xERC20.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
-contract Deb0xERC20 is ERC20 {
+
+contract Deb0xERC20 is ERC20Permit {
 
     address public owner;
 
-    constructor() ERC20("Deb0x Reward Token on Polygon", "pDBX") {
+    constructor() ERC20("Deb0x Reward Token on Polygon", "pDBX")
+    ERC20Permit("Deb0x Reward Token on Polygon") {
         owner = msg.sender;
     }
 
